0d7787999fc5410466996c81ba0c11e0a2076d09
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-10 22:17:16 +0300

    feat: Incluedes added

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<int:month>", views.montlyChallengeNumber),
     path("<str:month>", views.monthlyChallenge, name="month-challenge"),
 ]
